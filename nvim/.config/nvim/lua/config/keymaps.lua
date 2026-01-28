0e17725ffa0e87b08ef855e5f1964871288e45a5
julien <forevergoodforest@outlook.com>
2026-01-28 14:32:49 +0800

    fix(nvim): disable trouble plugin to fix the crash problem

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<ESC>", { desc = "离开" })
+vim.keymap.set("n", "<leader>xx", vim.diagnostic.setloclist, {
+  desc = "当前文件的错误",
+})
+vim.keymap.set("n", "<leader>xX", vim.diagnostic.setqflist, {
+  desc = "整个项目的错误",
+})
