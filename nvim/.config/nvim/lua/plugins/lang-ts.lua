69d2bd65a2ab850aa5ae71bf98c3bc30e9df0fa4
julien <forevergoodforest@outlook.com>
2026-02-11 13:11:41 +0800

    fix(nvim): disable astro lsp

--- nvim/.config/nvim/lua/plugins/lang-ts.lua
+++ nvim/.config/nvim/lua/plugins/lang-ts.lua
@@ -3,8 +3,9 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        vtsls = false, -- 禁用vtsls避免ts 5.9.3 bug，
+        -- vtsls = false, -- 禁用vtsls避免ts 5.9.3 bug，
         -- vtsls: -32603: Request textDocument/inlayHint failed with message: <semantic> TypeScript Server Error (5.9.3)
+        -- astro的ts lsp与ts的lsp冲突了，应该是版本不同的问题
       },
     },
   },
